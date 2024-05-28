7588a5cb1a62b6b5615858811c885b2605b1fe89
Danil129 <149506380+Danil129@users.noreply.github.com>
2024-05-28 17:23:39 +0300

    добавил фронтенд в папку publck

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
      // app.js 
      const express = require('express');
-     
-     // Импортируем роутеры
-     const mainRoute = require('./routes/main');
-     const gamesRouter = require('./routes/games'); 
+     const fs = require("fs").promises
+     // Импортируем роутер
+     const path = require('path');
+
      
      const PORT = 3000;
      const app = express();
      
      // Запускаем
-     app.use(mainRoute, gamesRouter); 
+     app.use(express.static(path.join(__dirname , "public"))); 
      
      app.listen(PORT, () => {
        console.log(`Server is running at PORT http://localhost:${PORT}`);
      })
+     app.get("/", (req, res) =>{
+      fs.readFile("./public/index.html","Utf-8").then((data) => {
+        res.header("Content-Type" , "text/html");
+        res.send(data);
+      });
+     } );
      
