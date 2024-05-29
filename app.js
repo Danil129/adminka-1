277bddd2601ba402c2f494d0cfb8c4c42bbf60ad
Danil129 <149506380+Danil129@users.noreply.github.com>
2024-05-29 12:56:17 +0300

    Дэзэшечка на проверку

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
+ // app.js 
+ const express = require('express');
+ const path = require('path');
+ // Импортируем библиотеку
+ const bodyParser = require('body-parser');
+ const mainRoute = require('./routes/main');
+ const gamesRouter = require('./routes/games'); 
  
-     const express = require('express');
-     const fs = require("fs").promises
-     const bodyParser = require("body-parser")
-     
-     const path = require('path');
-     const mainRoute = require('./routes/main');
-     const gamesRouter = require('./routes/games'); 
-    const cors = require('./middlewares/cors');
-     
-     const PORT = 3000;
-     const app = express();
-     
-     
-     app.use(
-      cors,
-      bodyParser.json() ,
-      express.static(path.join(__dirname , "public")),
-      mainRoute,
-      gamesRouter
-      ); 
-     
-     app.listen(PORT, () => {
-       console.log(`Server is running at PORT http://localhost:${PORT}`);
-      
-     })
-     app.get("/", (req, res) =>{
-      fs.readFile("./public/index.html","Utf-8").then((data) => {
-        res.header("Content-Type" , "text/html");
-        res.send(data);
-      });
-     } );
+ const PORT = 3000;
+ const app = express();
+ 
+ // Настраиваем миддлвар-функцию bodyParser
+ app.use(
+     bodyParser.json(),
+     express.static(path.join(__dirname, 'public')),
+     mainRoute,
+     gamesRouter
+ ); 
+ 
+ app.listen(PORT, () => {
+     console.log(`Server is running at PORT http://localhost:${PORT}`);
+ })
+ 
      
